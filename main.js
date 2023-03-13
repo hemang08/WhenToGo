f8b44fdcf7e586397a50e8240dfbfc3ec25c6f9d
sit310 <hemangdholu2002@gmail.com>
2023-03-13 10:44:06 +0530

    bug fixes

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ function cacl(){
     var out = document.getElementById("out").value.trim();
     var totalh = document.getElementById("totalh").value.trim();
 
-    var totalhour = totalh.split('H')[0]
-    var totalminute = parseInt(totalh.split('H ')[1].split('M')[0]) + (parseInt(totalhour) * 60);
+    try{
+        var totalhour = totalh.split('H')[0]
+        var totalminute = parseInt(totalh.split('H ')[1].split('M')[0]) + (parseInt(totalhour) * 60);
+    }catch{
+        var totalhour =0;
+        var totalminute = totalh.split('M')[0];
+    }
 
     var outhour =  parseInt(out.split(':')[0]);
     var outminute = parseInt(out.split(':')[1]);
@@ -17,32 +22,22 @@ function cacl(){
 
     var lastMinute = remaining%60;
     
-    
-
     outhour += 	lastHour;
     outminute += lastMinute;
     if(outminute>60){
         outhour++;
         outminute -=60;
     }
-    
-
-   
-    // console.log(lastHour);
-    // console.log(lastMinute);
 
     var d = new Date();
     let hour = d.getHours() <12 ? d.getHours() : d.getHours() - 12;
     
     let totalminute1 = d.getMinutes();
-    
-    console.log(hour, typeof(hour));
-    console.log(outhour, typeof(outhour));
     if(hour > outhour || (hour == outhour && totalminute1 > outminute) ){
-        alert("Nikad.....")
+        alert("You can leave, You already done with hours \nYour Today Hour is: " +hour+"H:"+totalminute1+"M");
     }
     else{
-        alert("Ubho ree \n"+ outhour+':'+outminute)
+        alert("Please  Wait Untill  \n"+ outhour+':'+outminute)
     }
 
 }
