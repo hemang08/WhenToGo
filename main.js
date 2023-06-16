e37446eb342b9c429a73f0fa192670b53a26d2da
Phoenix <chiragp462002@gmail.com>
2023-06-16 13:41:44 +0530

    reduce gif load time

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 var requiredTime;
+$(document).ready(function(){
+    $("#btncalc").off().click(function(){
+        cacl();
+    })
+})
+
+
 function cacl() {
     var out = document.getElementById("out").value.trim().toUpperCase();
     var totalh = document.getElementById("totalh").value.trim().toUpperCase();
@@ -49,57 +56,110 @@ function cacl() {
         outhour++;
         outminute -= 60;
     }
-
+    var rendomNum = Math.ceil(Math.random() * 49);
     var d = new Date();
     let hour = d.getHours() < 12 ? d.getHours() : d.getHours() - 12;
     let totalminute1 = d.getMinutes();
 
     if (hour > outhour || (hour == outhour && totalminute1 >= outminute)) {
-        fetch(`https://g.tenor.com/v1/search?q=gohome&key=LIVDSRZULELA&limit=50`).then((res) => {
-            return res.json()
-        }).then((data) => {
-            var obj = data.results[Math.ceil(Math.random() * 49)];
-            var str = obj.media[0].gif.url;
-            Swal.fire({
-                text: "You can leave, You already done with hours",
-                imageUrl: str || 'https://i.redd.it/d4bezp9infx41.png',
-                imageWidth: 400,
-                imageHeight: 200,
-                imageAlt: 'Custom image',
-                width: 600,
-                padding: '3em',
-                color: '#716add',
-                background: '#fff url(https://sweetalert2.github.io/images/trees.png)',
-                backdrop: `
-        rgba(0,0,123,0.4)`,
-                allowOutsideClick: false
-            })
-        })
+        $.ajax({
+            type: "Get",
+            url: "https://g.tenor.com/v1/search?q=gohome&key=LIVDSRZULELA&limit=50",
+            success: function (response) {
+                var GoGifUrl = response.results[rendomNum].media[0].gif.url;
+                Swal.fire({
+                            text: "You can leave, You already done with hours",
+                            imageUrl: GoGifUrl || 'https://i.redd.it/d4bezp9infx41.png',
+                            imageWidth: 400,
+                            imageHeight: 200,
+                            imageAlt: 'Custom image',
+                            width: 600,
+                            padding: '3em',
+                            color: '#716add',
+                            background: '#fff url(https://sweetalert2.github.io/images/trees.png)',
+                            backdrop: `
+                    rgba(0,0,123,0.4)`,
+                            allowOutsideClick: false
+                        })
+            }
+        });
+
+        //#region Old Code 
+        // fetch(`https://g.tenor.com/v1/search?q=gohome&key=LIVDSRZULELA&limit=50`).then((res) => {
+        //     return res.json()
+        // }).then((data) => {
+        //     var obj = data.results[Math.ceil(Math.random() * 49)];
+        //     var str = obj.media[0].gif.url;
+        //     Swal.fire({
+        //         text: "You can leave, You already done with hours",
+        //         imageUrl: str || 'https://i.redd.it/d4bezp9infx41.png',
+        //         imageWidth: 400,
+        //         imageHeight: 200,
+        //         imageAlt: 'Custom image',
+        //         width: 600,
+        //         padding: '3em',
+        //         color: '#716add',
+        //         background: '#fff url(https://sweetalert2.github.io/images/trees.png)',
+        //         backdrop: `
+        // rgba(0,0,123,0.4)`,
+        //         allowOutsideClick: false
+        //     })
+        // })
+        //#endregion
 
     }
     else {
         if (outhour > 12) {
             outhour -= 12;
         }
-        fetch(`https://g.tenor.com/v1/search?q=dontgo&key=LIVDSRZULELA&limit=50`).then((res) => {
-            return res.json()
-        }).then((data) => {
-            var obj = data.results[Math.ceil(Math.random() * 49)];
-            var str = obj.media[0].gif.url;
-            Swal.fire({
-                title: "Please  Wait Untill  \n" + (outhour < 10 ? ("0" + outhour) : outhour) + ':' + (outminute < 10 ? ("0" + outminute) : outminute),
-                imageUrl: str || './gallery/jaldi.jpg',
-                imageWidth: 400,
-                imageHeight: 200,
-                imageAlt: 'Custom image',
-                width: 600,
-                padding: '3em',
-                color: '#716add',
-                background: '#fff url(./gallery/O9FG4W0.jpg)',
-                backdrop: `
-                rgba(218, 218, 233, 0.72)`,
-                allowOutsideClick: false
-            })
+        
+        
+        $.ajax({
+            type: "GET",
+            url: "https://g.tenor.com/v1/search?q=gohome&key=LIVDSRZULELA&limit=50",
+            success: function (response) {
+                
+                let StayGifUrl = response.results[rendomNum].media[0].gif.url;
+                Swal.fire({
+                    title: "Please  Wait Untill  \n" + (outhour < 10 ? ("0" + outhour) : outhour) + ':' + (outminute < 10 ? ("0" + outminute) : outminute),
+                    imageUrl: StayGifUrl || './gallery/jaldi.jpg',
+                    imageWidth: 400,
+                    imageHeight: 200,
+                    imageAlt: 'Custom image',
+                    width: 600,
+                    padding: '3em',
+                    color: '#716add',
+                    background: '#fff url(./gallery/O9FG4W0.jpg)',
+                    backdrop: `
+                    rgba(218, 218, 233, 0.72)`,
+                    allowOutsideClick: false
+                })
+            },
+            error : function(error){
+                console.log(error)
+            }
         });
+        //#region  Old Code
+        // fetch(`https://g.tenor.com/v1/search?q=dontgo&key=LIVDSRZULELA&limit=50`).then((res) => {
+        //     return res.json()
+        // }).then((data) => {
+        //     var obj = data.results[Math.ceil(Math.random() * 49)];
+        //     var str = obj.media[0].gif.url; 
+            // Swal.fire({
+            //     title: "Please  Wait Untill  \n" + (outhour < 10 ? ("0" + outhour) : outhour) + ':' + (outminute < 10 ? ("0" + outminute) : outminute),
+            //     imageUrl: StayGifUrl,
+            //     imageWidth: 400,
+            //     imageHeight: 200,
+            //     imageAlt: 'Custom image',
+            //     width: 600,
+            //     padding: '3em',
+            //     color: '#716add',
+            //     background: '#fff url(./gallery/O9FG4W0.jpg)',
+            //     backdrop: `
+            //     rgba(218, 218, 233, 0.72)`,
+            //     allowOutsideClick: false
+            // })
+        // });
+        //#endregion
     }
 }
